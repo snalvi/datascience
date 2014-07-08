0ccefdf7ac403d8ad5aebcadfef18404aac71524
Saman A <saman.alvi01@gmail.com>
2014-07-07 23:36:21 -0400

    parsing out sentiment scores

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,21 @@
 import sys
+import simplejson
 
-def hw():
-    print 'Hello, world!'
+sentimentScores = {}
 
 def lines(fp):
     print str(len(fp.readlines()))
 
+def parseSentimentScores(sent_file):
+	for line in sent_file:
+		wordSentScoreMapping = map(lambda x: x.strip(), line.split('\t'))
+		sentimentScores[wordSentScoreMapping[0]] = int(wordSentScoreMapping[1])
+	
 def main():
     sent_file = open(sys.argv[1])
+    parseSentimentScores(sent_file)
+
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
 
 if __name__ == '__main__':
     main()
