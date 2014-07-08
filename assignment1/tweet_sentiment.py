019ee9a29ee752693425f4ab4a6bf4411d33b544
Saman A <saman.alvi01@gmail.com>
2014-07-08 00:35:13 -0400

    changed map(..) to use list comprehension instead as Python standard. Added in calculations for sentiment analysis on each word. Data cleaning needs to be reworked to be cleaner. Issues with certain words embedded within tweets with a larger dataset, but otherwise prints sentiment for a tweet or 0

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,5 +1,6 @@
 import sys
 import simplejson
+import json
 
 sentimentScores = {}
 
@@ -8,14 +9,32 @@ def lines(fp):
 
 def parseSentimentScores(sent_file):
 	for line in sent_file:
-		wordSentScoreMapping = map(lambda x: x.strip(), line.split('\t'))
+		# wordSentScoreMapping = map(lambda x: x.strip(), line.split('\t'))
+		wordSentScoreMapping = [word.strip() for word in line.split('\t')]
 		sentimentScores[wordSentScoreMapping[0]] = int(wordSentScoreMapping[1])
-	
+ 
+def calculateSentiments(tweet_file):
+	for line in tweet_file:
+
+		#cleaning data
+		line = line.replace('false', '\"False\"')
+		line = line.replace('true', '\"True\"')
+		line = line.replace(':null', ':\"\"')
+
+		tweetObj = json.loads(line)
+		if tweetObj.has_key('text'):
+			wordScores = map(lambda word: sentimentScores[word] if sentimentScores.has_key(word) else 0, tweetObj['text'].split(' '))
+			print sum(wordScores) #for readability
+
+		else: #no tweet text; so by default the sentiment is nothing
+			print 0
+
 def main():
     sent_file = open(sys.argv[1])
     parseSentimentScores(sent_file)
 
     tweet_file = open(sys.argv[2])
+    calculateSentiments(tweet_file)
 
 if __name__ == '__main__':
     main()
